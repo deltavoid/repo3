e762af37860349f19448b4d536d9cd2f26c0d8cc
root <zhangqianyu.sys@bytedance.com>
2021-09-18 19:48:23 +0800

    add library3

---
+++ library3/library3.c
@@ -0,0 +1,17 @@
+#include <repo3/library3/public/library3.h>
+// #include <repo2/library2/public/library2.h>
+// #include <repo1/library1/public/library1.h>
+
+#include <stdio.h>
+
+
+
+
+void repo3_library3_display()
+{
+    printf("repo3_library3_display: 1\n");
+
+    // repo1_library1_display();
+
+    printf("repo3_library3_display: 2\n");
+}
---
+++ library3/public/library3.h
@@ -0,0 +1,11 @@
+#ifndef REPO1_LIBRARY3_H
+#define REPO1_LIBRARY3_H
+
+
+
+extern void repo3_library3_display();
+
+
+
+
+#endif // REPO1_LIBRARY3_H
