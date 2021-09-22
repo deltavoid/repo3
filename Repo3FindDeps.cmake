6c2c6827cb87ed78021d5956d32de7084f5a75c6
root <zhangqianyu.sys@bytedance.com>
2021-09-22 21:45:16 +0800

    find_dependency

--- Repo3FindDeps.cmake
+++ Repo3FindDeps.cmake
@@ -2,7 +2,16 @@
 
 message("Repo3FindDeps.cmake begin")
 
-find_package(Repo2 REQUIRED)
+# find_package(Repo2 REQUIRED)
+
+
+
+include(CMakeFindDependencyMacro)
+
+
+find_dependency(Repo2 REQUIRED)
+
+
 
 if (NOT TARGET Repo2::library2)
   message("not find Repo2::library2")
