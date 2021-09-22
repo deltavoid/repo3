7b8bc85192bce824578b5c5703a32433510df709
root <zhangqianyu.sys@bytedance.com>
2021-09-22 15:08:41 +0800

    update Repo3Config.cmake.in

--- Repo3FindDeps.cmake
+++ Repo3FindDeps.cmake
@@ -1,8 +1,12 @@
 
 
+message("Repo3FindDeps.cmake begin")
+
 find_package(Repo2 REQUIRED)
 
 if (NOT TARGET Repo2::library2)
   message("not find Repo2::library2")
 endif ()
 
+
+message("Repo3FindDeps.cmake end")
